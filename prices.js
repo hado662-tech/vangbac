92828f04e1f4f1d10be304f2e8d2ea5bb0d938e2
hado662-tech <hado662@gmail.com>
2026-01-30 21:44:08 +0700

    OPTIMIZE: Remove Puppeteer, use Axios, Force Update

--- prices.js
+++ prices.js
@@ -3,7 +3,7 @@
 // Nguồn: BTMC, SJC, DOJI + tham khảo Kim Tín
 // ==========================================
 
-const LAST_UPDATE = "21:34 30/01/2026";
+const LAST_UPDATE = "21:44 30/01/2026";
 
 const GOLD_PRICES = {
     "quyTung": {
